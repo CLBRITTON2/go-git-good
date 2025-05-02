a5305c882d81563662e4fe716d1683b52a4e8728
CLBRITTON2 <christopher.l.britton@outlook.com>
2025-05-02 18:33:41 -0400

    Add find repo function, standardize directory checks (needs to be a util probably)

--- internal/repository.go
+++ internal/repository.go
@@ -31,7 +31,7 @@ func CreateRepository(path string) (*Repository, error) {
 		GitDirectory: filepath.Join(path, ".gitgood"),
 	}
 
-	if _, err := os.Stat(repository.GitDirectory); !os.IsNotExist(err) {
+	if info, err := os.Stat(repository.GitDirectory); err == nil && info.IsDir() {
 		return nil, fmt.Errorf("git repository already exists at %v", path)
 	}
 
@@ -63,11 +63,39 @@ func CreateRepository(path string) (*Repository, error) {
 		return nil, fmt.Errorf("error writing HEAD file: %v", err)
 	}
 
-	defaultDescriptionContents := []byte("Unnamed repository; edit 'description' file to name the repository\n")
+	defaultDescriptionContents := []byte("Unnamed repository: edit 'description' file to name the repository\n")
 	err = os.WriteFile(filepath.Join(repository.GitDirectory, "description"), defaultDescriptionContents, 0644)
 	if err != nil {
 		return nil, fmt.Errorf("error writing description file: %v", err)
 	}
 
 	return &repository, nil
+}
+
+func FindRepository(path string) (*Repository, error) {
+	if path == "" {
+		path = "."
+	}
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not resolve absolute path: %w", err)
+	}
+
+	gitDirectory := filepath.Join(path, ".gitgood")
+
+	if info, err := os.Stat(gitDirectory); err == nil && info.IsDir() {
+		repository := Repository{
+			WorkTree:     path,
+			GitDirectory: gitDirectory,
+		}
+		return &repository, nil
+	}
+
+	parentDirectory := filepath.Dir(path)
+
+	if parentDirectory == path {
+		return nil, fmt.Errorf("no git repository found")
+	}
+
+	return FindRepository(parentDirectory)
 }
