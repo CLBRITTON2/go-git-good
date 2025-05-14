07e9ca39519d488f5b42bdd5b7db1906061a9d16
CLBRITTON2 <christopher.l.britton@outlook.com>
2025-05-14 16:00:55 -0400

    Add -s flag for ls-files to mimic git ls-files -s for the most part

--- cmd/ls_files.go
+++ cmd/ls_files.go
@@ -7,9 +7,10 @@ import (
 )
 
 func LsFiles(flags []string) {
-	if len(flags) > 0 {
+	if len(flags) > 1 || (len(flags) == 1 && flags[0] != "-s") {
 		fmt.Println("Unsupported flag...")
 		printLsFilesUsage()
+		return
 	}
 
 	repository, err := common.FindRepository(".")
@@ -22,11 +23,19 @@ func LsFiles(flags []string) {
 		fmt.Printf("%v\n", err)
 	}
 
-	for _, entry := range index.Entries {
-		fmt.Printf("%v\n", entry.EntryPath)
+	if flags[0] == "-s" {
+		for _, entry := range index.Entries {
+			fmt.Printf("%o %v         %v\n", entry.FileMode, entry.Hash.String(), entry.EntryPath)
+			return
+		}
+
+		for _, entry := range index.Entries {
+			fmt.Printf("%v\n", entry.EntryPath)
+		}
 	}
 }
 
 func printLsFilesUsage() {
 	fmt.Println("Usage: gitgood ls-files          Show information about files in the index")
+	fmt.Println("Usage: gitgood ls-files -s       Show staged contents' mode bits, object hash, and index entry path")
 }
